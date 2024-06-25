2a93d163b5e49895d02710a2c946228db7a9a986
Chase-520 <chace070528@gmail.com>
2024-06-25 16:15:08 -0700

    testing graph function

---
+++ filled-animation-graph.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/TestGraph')
+def graph():
+    # Load your CSV file
+    # df = pd.read_csv('data.csv')
+
+    # Select subset of data for plotting
+    # time_subset = df.iloc[18:30, 0].tolist() # This data is limited to 11 data points for the Force and Time
+    # forcex_subset = df.iloc[18:30, 1].tolist()
+    # forcey_subset = df.iloc[18:30, 2].tolist()
+    # forcez_subset = df.iloc[18:30, 3].tolist()
+    return render_template('filled-area-animation.html')
+if __name__ == '__main__':
+    app.run(debug=True)
