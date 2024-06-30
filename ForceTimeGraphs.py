e0cca8a5d2f12864641b22fb88686cea691538fd
DerenDesign <67876073+DerenDesign@users.noreply.github.com>
2024-06-29 22:24:56 -0700

    .

--- ForceTimeGraphs.py
+++ ForceTimeGraphs.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,16 +14,14 @@ def graph():
     df = pd.read_csv('data/SM_SbS_02_Raw_Data - SM_SoftballSwing_Trial2_Raw_Data.csv')
 
     # Select subset of data for plotting
-    timex_subset = df.iloc[18:10000, 0].tolist() # This data is limited to 11 data points for the Force and Time
+    timex_subset = df.iloc[18:10000, 0].tolist()
     forcex_subset = df.iloc[18:10000, 1].tolist()
-
-    timey_subset = df.iloc[18:10000, 0].tolist() # This data is limited to 11 data points for the Force and Time
     forcey_subset = df.iloc[18:10000, 2].tolist()
 
     return render_template('graph.html', 
                            forcex=forcex_subset, 
-                           timey=timey_subset,
-                           forcey=forcey_subset, 
-                           timex=timex_subset)
+                           timex=timex_subset,
+                           forcey=forcey_subset)
+                           
 if __name__ == '__main__':
     app.run(debug=True)
