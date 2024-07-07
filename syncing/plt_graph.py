d5934da726b2e55839aa19a56ab4a34cf101d5db
Yodaman07 <ayaan.irshad@gmail.com>
2024-07-07 14:28:55 -0700

    The graph now animates!!!

--- syncing/plt_graph.py
+++ syncing/plt_graph.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import matplotlib.animation as anim
+from matplotlib.animation import FuncAnimation
 import pandas as pd
 
 
@@ -7,20 +7,52 @@ class Graph:
     def __init__(self, csv_path):
         self.csv_path = csv_path
         self.df = pd.read_csv(csv_path)
+        self.fps = 60
+        self.totalFrames = self.fps * 3  # seconds * fps
+        self.x_subset = None
+        self.y_subset = None
 
-    def graph(self, y_axis: {}):
-        timex_subset = self.df.iloc[18:10000, 0].astype(float).tolist()
+    def setData(self, x_axis_subset, y_axis_subset):
+        self.x_subset = x_axis_subset
+        self.y_subset = y_axis_subset
 
-        plt.plot(timex_subset, y_axis["data"])
-        plt.ylabel(y_axis["name"])
-        plt.xlabel("Time")
-        plt.xlim([0, 3])
-        plt.ylim([y_axis["min"], y_axis["max"]])
+    def animate(self, frame_num):
+        print(frame_num)
+        data_poins_count = len(self.x_subset["data"])
+        speedMult = round(data_poins_count / self.totalFrames)
 
-        plt.show()
+        frame_num *= speedMult
+
+        x_data = self.x_subset["data"][: frame_num]
+        y_data = self.y_subset["data"][: frame_num]
+
+        plt.plot(x_data, y_data)
+
+    def graph(self, animate = False, save = False):
+        fig, ax = plt.subplots()
+
+        plt.xlim([self.x_subset["min"], self.x_subset["max"]])
+        plt.ylim([self.y_subset["min"], self.y_subset["max"]])
+
+        plt.ylabel(self.y_subset["name"])
+        plt.xlabel(self.x_subset["name"])
+
+        if not animate:
+            self.animate(10000) # may not be accurate
+        else:
+            animation = FuncAnimation(fig, func=self.animate, frames=self.totalFrames, interval=1)
+            if save:
+                animation.save('syncing/graph.mp4', fps=self.fps)
+
+        if not save:
+            plt.show()
 
 
-g = Graph("data/SM_SbS_02_Raw_Data - SM_SoftballSwing_Trial2_Raw_Data.csv")
-# Select subset of data for plotting
-forcey_subset = {"data": g.df.iloc[18:10000, 2].astype(float).tolist(), "name": "ForceY", "min": -35, "max": 35}
-g.graph(forcey_subset)
+csv_name = "data/SM_SbS_02_Raw_Data - SM_SoftballSwing_Trial2_Raw_Data.csv"
+g = Graph(csv_name)
+
+forcey_subset = {"data": g.df.iloc[18:10000, 2].astype(float).tolist(), "name": "ForceY", "min": -15, "max": 15}
+timex_subset = {"data": g.df.iloc[18:10000, 0].astype(float).tolist(), "name": "Time", "min": 0, "max": 3}
+
+g.setData(timex_subset, forcey_subset)
+g.graph(True, True)
