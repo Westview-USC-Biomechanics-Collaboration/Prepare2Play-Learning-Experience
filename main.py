f952048f6f95631cc0b5a5eefe19d75641da7e71
DerenDesign <67876073+DerenDesign@users.noreply.github.com>
2024-07-11 22:53:30 -0700

    working on syncing graph with video. Was able to slow down graph but first frame is playing after delay

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import numpy as np
 
 app = Flask(__name__)
 
+def find_force_increase_start(force, threshold=4.5):
+    for i, f in enumerate(force):
+        if f > threshold:
+            return i
+    return 0
 
 @app.route('/')
 def index():
@@ -15,9 +20,18 @@ def graph():
     df = pd.read_csv('data/bjs_lr_DE_for01_Raw_Data - bjs_lr_DE_for01_Raw_Data.csv')
 
     # Select subset of data for plotting
-    timex_subset = df.iloc[18:10000, 0].astype(float).tolist()
-    forcex_subset = df.iloc[18:10000, 1].astype(float).tolist()
-    forcey_subset = df.iloc[18:10000, 2].astype(float).tolist()
+    timex = df.iloc[18:12000, 0].astype(float).tolist()
+    forcex = df.iloc[18:12000, 1].astype(float).tolist()
+    forcey = df.iloc[18:12000, 2].astype(float).tolist()
+
+    # Find the start of the force increase
+    start_index = find_force_increase_start(forcex)
+
+    # Subset the data starting from the force increase
+    timex_subset = timex[start_index:]
+    forcex_subset = forcex[start_index:]
+    forcey_subset = forcey[start_index:]
+
     integral_forcex = np.trapz(forcex_subset, timex_subset)
     integral_forcey = np.trapz(forcey_subset, timex_subset)
     formatted_integral_forcex = f"{integral_forcex:.2f}"
@@ -30,6 +44,6 @@ def graph():
                            forcey=forcey_subset,
                            integral_forcex=formatted_integral_forcex,
                            integral_forcey=formatted_integral_forcey)
-                           
+
 if __name__ == '__main__':
     app.run(debug=True)
