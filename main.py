f2d06a398834d2f6f6c5f8862c372279e09cd958
DerenDesign <67876073+DerenDesign@users.noreply.github.com>
2024-08-03 23:41:45 -0700

    .

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ def graph():
     # Find the start of the force increase
     start_index = find_force_increase_start(forcex)
 
+<<<<<<< Updated upstream
     # Subset the data starting from the force increase
     timex_subset = timex[start_index:]
     forcex_subset = forcex[start_index:]
@@ -88,6 +89,9 @@ def graph():
     integral_forcex = np.trapz(forcex_subset, timex_subset)
     integral_forcey = np.trapz(forcey_subset, timex_subset)
     formatted_integral_forcex = f"{integral_forcex:.2f}"
+=======
+    formatted_integral_forcex = f"{integral_forcex:.2f}" #.
+>>>>>>> Stashed changes
     formatted_integral_forcey = f"{integral_forcey:.2f}"
 
     # Render graph.html template and pass data to frontend
