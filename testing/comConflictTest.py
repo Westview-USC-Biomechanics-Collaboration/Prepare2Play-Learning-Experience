41151cf425f48e067c04386e7e563d3295600337
Chase <133188311+Chase-520@users.noreply.github.com>
2025-05-20 09:57:02 -0700

    start debugging import stuff

---
+++ testing/comConflictTest.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+import vector_overlay.stick_figure_COM  # replace with actual script name
+
+root = tk.Tk()
+label = tk.Label(root, text="Test GUI after import")
+label.pack()
+root.mainloop()
