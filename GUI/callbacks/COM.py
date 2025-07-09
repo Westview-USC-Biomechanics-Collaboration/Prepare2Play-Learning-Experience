cd823a15265671a3325c988a8793cc9f0d07c7e5
Chase <chace070528@gmail.com>
2025-07-08 18:50:37 -0700

    Fixed Center of Mass location

--- GUI/callbacks/COM.py
+++ GUI/callbacks/COM.py
@@ -11,7 +11,7 @@ def COMCallback(self):
         copyCam = cv2.VideoCapture(self.Video.path)
         copyCam.set(cv2.CAP_PROP_POS_FRAMES, 0)
         processor = Processor(self.Video.path)
-        processor.SaveToTxt(sex="m", filename="pose_landmarks.csv", confidencelevel=0.85, displayCOM=True)
+        processor.SaveToTxt(sex="f", filename="pose_landmarks.csv", confidencelevel=0.85, displayCOM=True)
         copyCam.release()
         del copyCam
         print("[INFO] COM processing finished.")
