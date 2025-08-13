b79b92b8e7e280bb4dbae6ba1c5a852bfb089b6f
DerenDesign <67876073+DerenDesign@users.noreply.github.com>
2025-08-12 17:15:26 -0700

    Adjust COM so that it starts lag frames after frame 0 to prevent COM from going off the athlete

--- GUI/callbacks/COM.py
+++ GUI/callbacks/COM.py
@@ -2,14 +2,19 @@ import tkinter as tk
 import threading
 import cv2
 from vector_overlay.stick_figure_COM import Processor
+from vector_overlay.vectoroverlay_GUI import VectorOverlay
 
 def COMCallback(self):
     print("[INFO] COM processing...")
     
     def threadTarget():
         print("[INFO] Starting COM processing in a separate thread...")
+        with open("lag.txt", "r") as f:
+            lag = int(f.read().strip())
+            print(lag)
+
         copyCam = cv2.VideoCapture(self.Video.path)
-        copyCam.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        copyCam.set(cv2.CAP_PROP_POS_FRAMES, lag)
         processor = Processor(copyCam)
         processor.SaveToTxt(sex="m", filename="pose_landmarks.csv", confidencelevel=0.85, displayCOM=True)
         copyCam.release()
