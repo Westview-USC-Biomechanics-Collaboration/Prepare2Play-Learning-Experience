929c18262bb79f4172fec9e4b2790b276f8f14a0
MaanyaShrivastava <maanya.shrivastava88@gmail.com>
2026-06-23 11:30:11 -0700

    Update save.py

--- GUI/callbacks/save.py
+++ GUI/callbacks/save.py
@@ -301,7 +301,11 @@ def saveCallback(self, video, view, frames):
 
             #Set y_lim to variable, which is used in all of the graphs as the y-maximum
             #ymax will be the same for all graphs so that they can be compared
-            y_lim = ymax*1.2
+
+            ymax = max(y1.max(), y2.max(), y3.max(), y4.max(), y5.max(), y6.max())
+            ymin = min(y1.min(), y2.min(), y3.min(), y4.min(), y5.min(), y6.min())
+
+            y_lim = ymax*1.2 
 
             fig1, ax1 = plt.subplots(figsize=(fig_width, fig_height), dpi=300)
             fig1.subplots_adjust(bottom=0.15, left=0.1, right=0.95, top=0.92)
@@ -325,6 +329,11 @@ def saveCallback(self, video, view, frames):
             fig2, ax2 = plt.subplots(figsize=(fig_width, fig_height), dpi=300)
             fig2.subplots_adjust(bottom=0.15, left=0.1, right=0.95, top=0.92)
 
+            ymax = max(y1.max(), y2.max(), y3.max(), y4.max(), y5.max(), y6.max())
+            ymin = min(y1.min(), y2.min(), y3.min(), y4.min(), y5.min(), y6.min())
+
+            y_lim = ymax*1.2
+
             plt.xticks(fontsize=4)
             plt.yticks(fontsize=4)
 
@@ -341,6 +350,11 @@ def saveCallback(self, video, view, frames):
         if view != "Top View":
             fig1, ax1 = plt.subplots(figsize=(fig_width, fig_height), dpi=300)
             fig1.subplots_adjust(bottom=0.15, left=0.1, right=0.95, top=0.92)
+
+            ymax = max(y1.max(), y2.max(), y3.max(), y4.max(), y5.max(), y6.max())
+            ymin = min(y1.min(), y2.min(), y3.min(), y4.min(), y5.min(), y6.min())
+
+            y_lim = ymax*1.2
 
             plt.xticks(fontsize=4)
             plt.yticks(fontsize=4)
@@ -359,6 +373,11 @@ def saveCallback(self, video, view, frames):
             # Figure 2: Force Plate 2
             fig2, ax2 = plt.subplots(figsize=(fig_width, fig_height), dpi=300)
             fig2.subplots_adjust(bottom=0.15, left=0.1, right=0.95, top=0.92)
+
+            ymax = max(y1.max(), y2.max(), y3.max(), y4.max(), y5.max(), y6.max())
+            ymin = min(y1.min(), y2.min(), y3.min(), y4.min(), y5.min(), y6.min())
+
+            y_lim = ymax*1.2
 
             plt.xticks(fontsize=4)
             plt.yticks(fontsize=4)
